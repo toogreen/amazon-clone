ff6bc531f3b85a16745951bceb0f5926c095dcb5
David Gagnon <toogreen@15inch.lan>
2020-09-09 20:15:50 -0400

    attempt to add flipmove and counter of items added in header

--- src/Checkout.js
+++ src/Checkout.js
@@ -3,9 +3,26 @@ import "./Checkout.css";
 import Subtotal from "./Subtotal";
 import { useStateValue } from "./StateProvider"
 import CheckoutProduct from "./CheckoutProduct"
+import FlipMove from 'react-flip-move';
 
 function Checkout({ id, title, image, price, rating }){
     const  [{ basket, user }, dispatch] = useStateValue();
+
+    const CheckoutProducts = ({ item }) => (
+        <FlipMove>
+            {basket.map(item => (
+            <CheckoutProduct
+                key={item.id}
+                id={item.id}
+                title={item.title}
+                image={item.image}
+                price={item.price}
+                rating={item.rating}
+            />
+            ))}
+        </FlipMove>
+    );
+
     return(
         <div className="checkout">
             <div className="checkout__left">
@@ -16,15 +33,7 @@ function Checkout({ id, title, image, price, rating }){
                     <h2 className="checkout__title">Your shopping Basket</h2>
                 </div>
 
-                {basket.map(item => (
-                <CheckoutProduct 
-                    id={item.id}
-                    title={item.title}
-                    image={item.image}
-                    price={item.price}
-                    rating={item.rating}
-                />
-                ))}
+                <CheckoutProducts />
 
             </div>
             <div className="checkout__right">
