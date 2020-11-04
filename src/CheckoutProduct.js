0c1488c0beea4c62302f3873bade501d15da57f5
David Gagnon <toogreen@gmail.com>
2020-11-03 20:48:46 -0500

    fixed CSS for checkout items using both grid and flexbox

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -15,9 +15,10 @@ function CheckoutProduct({ id, image, title, price, rating, hideButton }) {
     }
 
     return (
-        <div clasName='checkoutProduct'>
-            <img className='checkoutProduct__image' src={image} alt='' />
-
+        <div className='checkoutProduct'>
+            <div className='checkoutProduct__image'>
+                <img src={image} alt='' />
+            </div>
             <div className="checkoutProduct__info">
                 <p className="checkoutProduct__title">{title}</p>
                 <p className="checkoutProduct__price">
